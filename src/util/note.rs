6eb9d29ede36d9f4406d86983c9e90f19299a334
SICARIOXSICARIO <jwolfe27314@gmail.com>
2023-12-10 03:00:22 -0700

    working

--- src/util/note.rs
+++ src/util/note.rs
@@ -1,14 +1,31 @@
-pub enum Note {
-    A,
-    ASharp,
-    B,
-    C,
-    CSharp,
-    D,
-    DSharp,
-    E,
-    F,
-    FSharp,
-    G,
-    GSharp,
+pub mod note {
+
+    use strum_macros::EnumString;
+    #[derive(EnumString)]
+    pub enum Note {
+        #[strum(serialize = "A")]
+        A,
+        #[strum(serialize = "A#")]
+        ASharp,
+        #[strum(serialize = "B")]
+        B,
+        #[strum(serialize = "C")]
+        C,
+        #[strum(serialize = "C#")]
+        CSharp,
+        #[strum(serialize = "D")]
+        D,
+        #[strum(serialize = "D#")]
+        DSharp,
+        #[strum(serialize = "E")]
+        E,
+        #[strum(serialize = "F")]
+        F,
+        #[strum(serialize = "F#")]
+        FSharp,
+        #[strum(serialize = "G")]
+        G,
+        #[strum(serialize = "G#")]
+        GSharp,
+    }
 }
